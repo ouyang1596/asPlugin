1253caef321e5bdf85490425ee8aa3f60a7a4c50
欧阳毅军 <oyangyijun@iicall.com>
2023-12-08 17:33:43 +0800

    新增kotlin代码模板

--- src/main/java/com/ouyang/codetemplategenerator/CodeTemplateGenerator.java
+++ src/main/java/com/ouyang/codetemplategenerator/CodeTemplateGenerator.java
@@ -17,18 +17,42 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
-
+/**
+ * 代码模版生成器
+ */
 public class CodeTemplateGenerator extends AnAction {
+
+    private Project project;
+    /**
+     * 类的名字
+     */
+    private String className;
+    /**
+     * 布局文件名字
+     */
+    private String layoutName;
+    /**
+     * 文件格式
+     */
+    private String fileType;
+    /**
+     * 点击的文件夹位置相关类
+     */
+    private VirtualFile virtualFile;
+    /**
+     * 点击的文件夹位置路径
+     */
+    private String targetFolderPath;
 
     @Override
     public void actionPerformed(AnActionEvent e) {
-        Project project = e.getProject();
+        project = e.getProject();
         if (project == null) {
             return;
         }
 
         // 获取点击的文件夹位置
-        VirtualFile virtualFile = (VirtualFile) e.getDataContext().getData("virtualFile");
+        virtualFile = (VirtualFile) e.getDataContext().getData("virtualFile");
         if (virtualFile == null || !virtualFile.isDirectory()) {
             return;
         }
@@ -41,68 +65,118 @@ public class CodeTemplateGenerator extends AnAction {
             return;
         }
 
-        String className = dialog.getClassName();
-        String layoutName = "binder_" + className.replace("Binder", "").toLowerCase();
+        className = dialog.getClassName();
+        layoutName = "binder_" + className.replace("Binder", "").toLowerCase();
         String data = className.replace("Binder", "") + "Data";
-        String fileType = dialog.getFileType();
+        fileType = dialog.getFileType();
 
-        // 在点击的文件夹位置下创建一个新文件
-        String targetFolderPath = virtualFile.getPath();
-        String targetFilePath = targetFolderPath + "/" + className + "." + fileType;
+        targetFolderPath = virtualFile.getPath();
+        String form;
+        if (InputDialog.FileType.JAVA.equals(fileType)) {
+            form = "java";
+        } else {
+            form = "kt";
+        }
+        String targetFilePath = targetFolderPath + "/" + className + "." + form;
 
-        generateCode(project, virtualFile, className, layoutName, data, targetFolderPath, targetFilePath);
-        generateLayout(project, virtualFile, layoutName, targetFolderPath);
+        generateCode(data, targetFilePath);
+        generateLayout();
     }
 
     /**
      * 生成代码
      */
-    private void generateCode(Project project, VirtualFile virtualFile, String className, String layoutName, String data, String targetFolderPath, String targetFilePath) {
+    private void generateCode(String data, String targetFilePath) {
         try {
 
             // 将路径转换为包名
             String packageName = convertToPackageName(targetFolderPath);
+            String code;
+            if (InputDialog.FileType.JAVA.equals(fileType)) {
+                code = "package " + packageName + ";\n" +
+                        "\n" +
+                        "import android.view.LayoutInflater;\n" +
+                        "import android.view.View;\n" +
+                        "import android.view.ViewGroup;\n" +
+                        "import android.widget.TextView;\n" +
+                        "\n" +
+                        "import androidx.annotation.NonNull;\n" +
+                        "import androidx.recyclerview.widget.RecyclerView;\n" +
+                        "\n" +
+                        "import com.ailiao.mosheng.commonlibrary.view.BaseItemViewBinder;\n" +
+                        "import com.mosheng.R;\n" +
+                        "\n" +
+                        "import java.io.Serializable;\n" +
+                        "\n" +
+                        "public class " + className + " extends BaseItemViewBinder<" + className + "." + data + ", " + className + ".ViewHolder> {\n" +
+                        "    private static final String TAG = \"" + className + "\";\n" +
+                        "\n" +
+                        "    @NonNull\n" +
+                        "    @Override\n" +
+                        "    protected ViewHolder onCreateViewHolder(@NonNull LayoutInflater inflater, @NonNull ViewGroup parent) {\n" +
+                        "        View view = inflater.inflate(R.layout."+layoutName+", parent, false);\n" +
+                        "        return new ViewHolder(view);\n" +
+                        "    }\n" +
+                        "\n" +
+                        "    @Override\n" +
+                        "    protected void onBindViewHolder(@NonNull final ViewHolder holder, @NonNull final " + data + " item) {\n" +
+                        "        holder.textView.setText(\"xxxx\");\n" +
+                        "    }\n" +
+                        "\n" +
+                        "    public class ViewHolder extends RecyclerView.ViewHolder {\n" +
+                        "        TextView textView;\n" +
+                        "\n" +
+                        "        ViewHolder(View itemView) {\n" +
+                        "            super(itemView);\n" +
+                        "            textView = itemView.findViewById(R.id.textView);\n" +
+                        "        }\n" +
+                        "    }\n" +
+                        "\n" +
+                        "    public static class " + data + " implements Serializable {\n" +
+                        "\n" +
+                        "    }\n" +
+                        "}";
+            } else {
+                code = "package " + packageName + "\n" +
+                        "\n" +
+                        "import android.view.LayoutInflater\n" +
+                        "import android.view.View\n" +
+                        "import android.view.ViewGroup\n" +
+                        "import androidx.recyclerview.widget.RecyclerView\n" +
+                        "import com.ailiao.mosheng.commonlibrary.view.BaseItemViewBinder\n" +
+                        "import com.mosheng.R\n" +
+                        "import kotlinx.android.synthetic.main." + layoutName + ".view.*\n" +
+                        "import java.io.Serializable\n" +
+                        "\n" +
+                        "class " + className + " :\n" +
+                        "    BaseItemViewBinder<" + className + "." + data + ", " + className + "." + "ViewHolder>() {\n" +
+                        "\n" +
+                        "    override fun onCreateViewHolder(inflater: LayoutInflater, parent: ViewGroup): ViewHolder {\n" +
+                        "        return ViewHolder(inflater.inflate(R.layout." + layoutName + ", parent, false))\n" +
+                        "    }\n" +
+                        "\n" +
+                        "    override fun onBindViewHolder(\n" +
+                        "        holder: ViewHolder,\n" +
+                        "        data: " + data + "\n" +
+                        "    ) {\n" +
+                        "        holder.itemView.textView.text = \"xxx\"\n" +
+                        "    }\n" +
+                        "\n" +
+                        "\n" +
+                        "    class ViewHolder internal constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {\n" +
+                        "\n" +
+                        "    }\n" +
+                        "\n" +
+                        "    class " + data + " : Serializable {\n" +
+                        "\n" +
+                        "    }\n" +
+                        "\n" +
+                        "}";
+            }
 
             File targetFile = new File(targetFilePath);
             FileWriter writer = new FileWriter(targetFile);
-            writer.write("package " + packageName + ";\n" +
-                    "\n" +
-                    "import android.view.LayoutInflater;\n" +
-                    "import android.view.View;\n" +
-                    "import android.view.ViewGroup;\n" +
-                    "\n" +
-                    "import androidx.annotation.NonNull;\n" +
-                    "import androidx.recyclerview.widget.RecyclerView;\n" +
-                    "\n" +
-                    "import com.ailiao.mosheng.commonlibrary.view.BaseItemViewBinder;\n" +
-                    "\n" +
-                    "import java.io.Serializable;\n" +
-                    "\n" +
-                    "public class " + className + " extends BaseItemViewBinder<" + className + "." + data + ", "+className + "." + "ViewHolder> {\n" +
-                    "    private static final String TAG = \"TestBinder\";\n" +
-                    "\n" +
-                    "    @NonNull\n" +
-                    "    @Override\n" +
-                    "    protected ViewHolder onCreateViewHolder(@NonNull LayoutInflater inflater, @NonNull ViewGroup parent) {\n" +
-                    "        View view = inflater.inflate(R.layout." + layoutName + ", parent, false);\n" +
-                    "        return new ViewHolder(view);\n" +
-                    "    }\n" +
-                    "\n" +
-                    "    @Override\n" +
-                    "    protected void onBindViewHolder(@NonNull final ViewHolder holder, @NonNull final " + data + " item) {\n" +
-                    "    }\n" +
-                    "\n" +
-                    "    public class ViewHolder extends RecyclerView.ViewHolder {\n" +
-                    "\n" +
-                    "        ViewHolder(View itemView) {\n" +
-                    "            super(itemView);\n" +
-                    "        }\n" +
-                    "    }\n" +
-                    "\n" +
-                    "    public static class " + data + "  implements Serializable {\n" +
-                    "\n" +
-                    "    }\n" +
-                    "}");
+            writer.write(code);
 //            writer.write("public class " + className + " {\n");
 //            writer.write("    // 生成的代码\n");
 //            writer.write("}\n");
@@ -121,28 +195,14 @@ public class CodeTemplateGenerator extends AnAction {
     /**
      * 生成对应的布局
      */
-    private void generateLayout(Project project, VirtualFile virtualFile, String layoutName, String targetFolderPath) {
+    private void generateLayout() {
         try {
 
             targetFolderPath = targetFolderPath.substring(0, targetFolderPath.indexOf("/java"));
             targetFolderPath += "/res/layout";
             String targetFilePath = targetFolderPath + "/" + layoutName + ".xml";
 
-            String xmlCode = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
-                    "<androidx.constraintlayout.widget.ConstraintLayout xmlns:android=\"http://schemas.android.com/apk/res/android\"\n" +
-                    "    xmlns:app=\"http://schemas.android.com/apk/res-auto\"\n" +
-                    "    xmlns:tools=\"http://schemas.android.com/tools\"\n" +
-                    "    android:layout_width=\"match_parent\"\n" +
-                    "    android:layout_height=\"wrap_content\">\n" +
-                    "    \n" +
-                    "    <TextView\n" +
-                    "        android:id=\"@+id/tv\"\n" +
-                    "        android:layout_width=\"wrap_content\"\n" +
-                    "        android:layout_height=\"wrap_content\"\n" +
-                    "        app:layout_constraintLeft_toLeftOf=\"parent\"\n" +
-                    "        app:layout_constraintTop_toTopOf=\"parent\"\n" +
-                    "        tools:text=\"xxx\" />\n" +
-                    "</androidx.constraintlayout.widget.ConstraintLayout>";
+            String xmlCode = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" + "<androidx.constraintlayout.widget.ConstraintLayout xmlns:android=\"http://schemas.android.com/apk/res/android\"\n" + "    xmlns:app=\"http://schemas.android.com/apk/res-auto\"\n" + "    xmlns:tools=\"http://schemas.android.com/tools\"\n" + "    android:layout_width=\"match_parent\"\n" + "    android:layout_height=\"wrap_content\">\n" + "    \n" + "    <TextView\n" + "        android:id=\"@+id/textView\"\n" + "        android:layout_width=\"wrap_content\"\n" + "        android:layout_height=\"wrap_content\"\n" + "        app:layout_constraintLeft_toLeftOf=\"parent\"\n" + "        app:layout_constraintTop_toTopOf=\"parent\"\n" + "        tools:text=\"xxx\" />\n" + "</androidx.constraintlayout.widget.ConstraintLayout>";
 
             File targetFile = new File(targetFilePath);
             FileWriter writer = new FileWriter(targetFile);
@@ -163,6 +223,10 @@ public class CodeTemplateGenerator extends AnAction {
     }
 
     private static class InputDialog extends DialogWrapper {
+        interface FileType {
+            String JAVA = "java";
+            String KOTLIN = "kotlin";
+        }
 
         private JTextField classNameField;
         private JComboBox<String> fileTypeComboBox;
@@ -186,7 +250,7 @@ public class CodeTemplateGenerator extends AnAction {
 
             JPanel fileTypePanel = new JPanel(new BorderLayout());
             JLabel fileTypeLabel = new JLabel("File Type:");
-            fileTypeComboBox = new JComboBox<>(new String[]{"java", "kotlin"});
+            fileTypeComboBox = new JComboBox<>(new String[]{FileType.KOTLIN, FileType.JAVA});
             fileTypePanel.add(fileTypeLabel, BorderLayout.WEST);
             fileTypePanel.add(fileTypeComboBox, BorderLayout.CENTER);
 
---
+++ src/main/java/com/ouyang/codetemplategenerator/utils/GenerateHelper.java
@@ -0,0 +1,5 @@
+package com.ouyang.codetemplategenerator.utils;
+
+public class GenerateHelper {
+
+}
